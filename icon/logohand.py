90d6d27238238b554ebee76d7cb524f582939e6d
Maxwell Dergosits <maxwelldergosits@gmail.com>
2025-09-04 08:59:15 -0600

    logo work

--- icon/logohand.py
+++ icon/logohand.py
@@ -1,50 +1,23 @@
 from cairosvg import svg2png
 
-otherContent = """
-      <g id="photo" transform="scale({contentScale}) translate(220 80)">
-        <!--
-        <g id="plant" transform="translate(22 0)">
-          <path d="
-            M138.54,149.46C106.62,96.25,149.18,43.05,239.63,48.37,245,138.82,191.75,181.38,138.54,149.46Z
-            M88.47,160.47c22.8-38-7.6-76-72.21-72.21C12.46,152.87,50.47,183.27,88.47,160.47Z
-            M200,88l-61.25,61.25A64,64,0,0,0,120,194.51V224
-            M56,128 L120,192
-          " fill="none" stroke="{stroke}" stroke-linecap="round" stroke-linejoin="round" stroke-width="{strokeWidth/contentScale}"
-          />
-        </g>
-        -->
-        <!--
-        <g id="mountain" transform="translate(22 0)">
-          <circle cx="164" cy="52" r="20" fill="none" stroke="{stroke}" stroke-linecap="round" stroke-linejoin="round" stroke-width="{strokeWidth/contentScale}"/>
-          <path d="M8,200,81.1,75.94a8,8,0,0,1,13.8,0L168,200Z" fill="none" stroke="{stroke}" stroke-linecap="round" stroke-linejoin="round" stroke-width="{strokeWidth/contentScale}"/>
-          <line x1="50.35" y1="128" x2="125.65" y2="128" fill="none" stroke="{stroke}" stroke-linecap="round" stroke-linejoin="round" stroke-width="{strokeWidth/contentScale}"/>
-          <path d="M146.61,163.71l33.06-55.79a8,8,0,0,1,13.76,0L248,200H168" fill="none" stroke="{stroke}" stroke-linecap="round" stroke-linejoin="round" stroke-width="{strokeWidth/contentScale}"/>
-        </g>
-        -->
-        <!--
-        <g id="person" transform="translate(22 -5)">
-          <rect width="256" height="256" fill="none"/>
-          <circle cx="152" cy="48" r="24" fill="none" stroke="{stroke}" stroke-linecap="round" stroke-linejoin="round" stroke-width="{strokeWidth/contentScale}"/>
-          <path d="M48,129s56-52.65,88-24.87C153.94,119.67,168,144,208,144" fill="none" stroke="{stroke}" stroke-linecap="round" stroke-linejoin="round" stroke-width="{strokeWidth/contentScale}"/>
-          <polyline points="152 232 152 176 109.54 145.67" fill="none" stroke="{stroke}" stroke-linecap="round" stroke-linejoin="round" stroke-width="{strokeWidth/contentScale}"/>
-          <line x1="129.53" y1="99.69" x2="72" y2="232" fill="none" stroke="{stroke}" stroke-linecap="round" stroke-linejoin="round" stroke-width="{strokeWidth/contentScale}"/>
-        </g>
-        -->
-      </g>
-      """
-
 strokeWidth = 8
 contentScale = .5
+#themeColorA = "#f55d42"
+#themeColorB = "#f5b641"
+themeColorA = "#85A98F"
+themeColorB = "#D3F1DF"
+
 stroke = "url(#fade)"
 svg = f"""
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000"
   xmlns:xlink="http://www.w3.org/1999/xlink">
   <defs>
     <linearGradient id="fade" x1="5%" y1="0%" x2="25%" y2="0%" gradientUnits="userSpaceOnUse">
-    <stop offset="0%" style="stop-color:#f55d42;stop-opacity:1" />
-    <stop offset="100%" style="stop-color:#f5b641;stop-opacity:1" />
+    <stop offset="0%" style="stop-color:{themeColorA};stop-opacity:1" />
+    <stop offset="100%" style="stop-color:{themeColorB};stop-opacity:1" />
     </linearGradient>
   </defs>
+  <!-- <rect x="0" y="0" height="100%" width="100%" fill="none" stroke="black" stroke-width="10"/> -->
   <g transform="scale(3.9)">
       <g id="grab" transform="translate(0 0)">
       <path d="
@@ -57,20 +30,21 @@ svg = f"""
       l-22.26 -34
       a20,20,0,0,1,34.64 -20
       l6.58 16.31
-      v-160
+      v-120
       a8,8,0,0,0-8-8
       h-120
       a8,8,0,0,0,-8,8
-      v180
+      v120
       a8,8,0,0,0,8,8
-      h100
+      h87
       " fill="none" stroke="{stroke}" stroke-linecap="round" stroke-linejoin="round" stroke-width="{strokeWidth}"/>
       </g>
       <g id="photo" fill="{stroke}" fill-rule="nonzero" >
         <path
         d="
-        M168,156
-        h-80
+        M168,{128 + 36 + 6}
+        m-10 0
+        h-70
         a8,8,0,0,1-8-8
         v-56
         a8,8,0,0,1,8,-8
@@ -81,9 +55,8 @@ svg = f"""
         h16
         a8,8,0,0,1,8,8
         v56
-        a8,8,0,0,1,-8,8
         z
-        m-65,-36
+        m-55,-36
         a 25 25 0 0 0 50 0
         a 25 25 0 0 0 -50 0
         m9,0
